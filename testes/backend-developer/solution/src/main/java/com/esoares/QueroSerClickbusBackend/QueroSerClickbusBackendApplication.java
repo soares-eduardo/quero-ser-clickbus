ae76cb2dbfcc81c1124b2f26f05c9a213337cb12
Eduardo Soares <eduardoandre.mcc@gmail.com>
2021-04-15 11:20:14 -0300

    Import basic files.

---
+++ testes/backend-developer/solution/src/main/java/com/esoares/QueroSerClickbusBackend/QueroSerClickbusBackendApplication.java
@@ -0,0 +1,21 @@
+package com.esoares.QueroSerClickbusBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class QueroSerClickbusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueroSerClickbusBackendApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello() {
+		return "Hello World";
+	}
+
+}
---
+++ testes/backend-developer/solution/src/test/java/com/esoares/QueroSerClickbusBackend/QueroSerClickbusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esoares.QueroSerClickbusBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueroSerClickbusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

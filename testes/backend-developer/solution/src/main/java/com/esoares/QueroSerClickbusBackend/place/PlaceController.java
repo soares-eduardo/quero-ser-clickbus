b40778b6add6f85c4e566e95a598f46a95fd8d8d
Eduardo Soares <eduardoandre.mcc@gmail.com>
2021-04-17 11:21:50 -0300

    Created the place datebase.API Successfully connected to MySQL database.

--- testes/backend-developer/solution/src/main/java/com/esoares/QueroSerClickbusBackend/place/PlaceController.java
+++ testes/backend-developer/solution/src/main/java/com/esoares/QueroSerClickbusBackend/place/PlaceController.java
@@ -2,6 +2,7 @@ package com.esoares.QueroSerClickbusBackend.place;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class PlaceController {
 
     private final PlaceService placeService;
 
+    @Autowired
     public PlaceController(PlaceService placeService) {
         this.placeService = placeService;
     }
--- testes/backend-developer/solution/src/main/java/com/esoares/QueroSerClickbusBackend/place/PlaceService.java
+++ testes/backend-developer/solution/src/main/java/com/esoares/QueroSerClickbusBackend/place/PlaceService.java
@@ -3,6 +3,9 @@ package com.esoares.QueroSerClickbusBackend.place;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PlaceService {
 
     public List<Place> getPlaces() {

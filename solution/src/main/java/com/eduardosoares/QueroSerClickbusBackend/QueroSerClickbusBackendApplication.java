e2861da55e195db0402d89f98bb7001816637d99
Eduardo André Soares <eduardoandre.mcc@gmail.com>
2021-07-15 20:46:51 -0300

    Created a new Spring Boot project.

--- solution/src/main/java/com/eduardosoares/QueroSerClickbusBackend/QueroSerClickbusBackendApplication.java
+++ solution/src/main/java/com/eduardosoares/QueroSerClickbusBackend/QueroSerClickbusBackendApplication.java
@@ -1,4 +1,4 @@
-package com.esoares.QueroSerClickbusBackend;
+package com.eduardosoares.QueroSerClickbusBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,4 +9,5 @@ public class QueroSerClickbusBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QueroSerClickbusBackendApplication.class, args);
 	}
+
 }
--- solution/src/test/java/com/eduardosoares/QueroSerClickbusBackend/QueroSerClickbusBackendApplicationTests.java
+++ solution/src/test/java/com/eduardosoares/QueroSerClickbusBackend/QueroSerClickbusBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.esoares.QueroSerClickbusBackend;
+package com.eduardosoares.QueroSerClickbusBackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

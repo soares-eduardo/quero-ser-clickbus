e8bf62f41ac83b4520e027026cfe533014d0a2b2
Eduardo André Soares <eduardoandre.mcc@gmail.com>
2021-07-16 01:28:44 -0300

    Created Controller, Place DTO, model and its table.

---
+++ solution/src/main/java/com/eduardosoares/QueroSerClickbusBackend/Application/Dtos/PlaceDTO.java
@@ -0,0 +1,61 @@
+package com.eduardosoares.QueroSerClickbusBackend.Application.Dtos;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+public class PlaceDTO {
+
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String slug;
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String state;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
---
+++ solution/src/main/java/com/eduardosoares/QueroSerClickbusBackend/Interface/PlaceController.java
@@ -0,0 +1,46 @@
+package com.eduardosoares.QueroSerClickbusBackend.Interface;
+
+import com.eduardosoares.QueroSerClickbusBackend.Application.Dtos.PlaceDTO;
+import org.apache.coyote.Response;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.print.attribute.standard.Media;
+import javax.validation.Valid;
+
+@CrossOrigin(value = "*")
+@RequestMapping(value = "/place")
+@RestController
+public class PlaceController {
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PlaceDTO> getPlaces() {
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping(value = "/{slug}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PlaceDTO> getPlaceBySlug(@PathVariable(value = "slug") String slug) {
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping(value = "/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PlaceDTO> getPlacesByName(@PathVariable(value = "name") String city) {
+        return ResponseEntity.ok(null);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PlaceDTO> registerNewPlace(@Valid @RequestBody PlaceDTO body) {
+        return ResponseEntity.ok(body);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PlaceDTO> updatePlace(@Valid @RequestBody PlaceDTO body) {
+        return ResponseEntity.ok(body);
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PlaceDTO> deletePlace(@PathVariable(value = "id") Long id) {
+        return ResponseEntity.ok(null);
+    }
+}

3b9806a9948e349711bc59ddb16d13c1f61599b2
Eduardo André Soares <eduardoandre.mcc@gmail.com>
2021-07-20 01:35:26 -0300

    Added a private constructor to utils

--- solution/src/main/java/com/eduardosoares/QueroSerClickbusBackend/utils/MessageUtils.java
+++ solution/src/main/java/com/eduardosoares/QueroSerClickbusBackend/utils/MessageUtils.java
@@ -2,6 +2,10 @@ package com.eduardosoares.QueroSerClickbusBackend.utils;
 
 public class MessageUtils {
 
+    private MessageUtils() {
+
+    }
+
     public static final String NO_RECORDS_FOUND = "No records found";
     public static final String PLACE_NOT_FOUND = "Place not found";
     public static final String PLACE_ALREADY_EXISTS = "Place already exists in the database";
